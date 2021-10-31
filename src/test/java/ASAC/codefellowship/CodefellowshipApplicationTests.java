ba328ad784d4fafd32a4aa462d985b5e3c61df8e
yazanabdulhafez <yazanali31@yahoo.com>
2021-10-31 22:09:03 +0200

    finish lab 16

---
+++ src/test/java/ASAC/codefellowship/CodefellowshipApplicationTests.java
@@ -0,0 +1,13 @@
+package ASAC.codefellowship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefellowshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
